e112ed915ecc8ad281f41eb3391f918782cf40f2
Chung-Wen Lin <joany.lin@gmail.com>
2021-06-07 23:41:23 +0200

    update ortholog issue of GWAS hits

--- covid19_gwasHit_random_v2.r
+++ covid19_gwasHit_random_v2.r
@@ -18,6 +18,7 @@ go2check <- function(x, organism) {
     return(goquery$result)
 }
 
+trace("plot.igraph", edit = T)
 ######
 # load dataset
 huri <- read.csv("~/Documents/INET-work/references/HuRI_binaryPPI/HuRI_Tong_withSymbol.csv", header = T)
@@ -27,6 +28,7 @@ gwas_2 <- read.xlsx("~/Documents/INET-work/virus_network/statistic_results/GWAS/
     # the gwas_2 was generated after 1st analysis
 gwas_file <- "~/Documents/INET-work/virus_network/statistic_results/GWAS/COVID_GWAS hits_source_v2.xlsx"
 gwas_candidate <- read.xlsx(gwas_file, sheet = "all")
+gwas_ortholog <- read.xlsx(gwas_file, sheet = "Ortholog")
 gwas_ctcl <- read.xlsx(gwas_file, sheet = "critical")
 gwas_hosp <- read.xlsx(gwas_file, sheet = "hospitalization")
 gwas_infct <- read.xlsx(gwas_file, sheet = "infection")
@@ -63,50 +65,12 @@ combineNetwork <- function(network, node) {
 gwas_hit_1st <- make_ego_graph(huri_g, nodes = sort(gwas$All.LD[gwas$All.LD %in% V(huri_g)$name]), order = 1, mode = "all") #17 of 42 in HuRI
 gwas_check <- length(unique(unlist(lapply
     (gwas_hit_1st, checkUpdate)))) #11 interactors
-gwas_all_candidate <- expand.grid(strsplit(gwas_candidate[, 5], split = ","))
-gwas_all_candidate_uniq <- unname(unique(as.matrix(unlist(gwas_all_candidate)))[, 1])
-gwas_all_candidate_huri <- gwas_all_candidate_uniq[gwas_all_candidate_uniq %in% V(huri_g)$name]
-gwas_ctcl_candidate <- expand.grid(strsplit(gwas_ctcl[, 5], split = ","))
-gwas_ctcl_candidate_uniq <- unname(unique(as.matrix(unlist(gwas_ctcl_candidate)))[, 1])
-gwas_ctcl_candidate_huri <- gwas_ctcl_candidate_uniq[gwas_ctcl_candidate_uniq %in% V(huri_g)$name]
-gwas_hosp_candidate <- expand.grid(strsplit(gwas_hosp[, 5], split = ","))
-gwas_hosp_candidate_uniq <- unname(unique(as.matrix(unlist(gwas_hosp_candidate)))[, 1])
-gwas_hosp_candidate_huri <- gwas_hosp_candidate_uniq[gwas_hosp_candidate_uniq %in% V(huri_g)$name]
-gwas_infct_candidate <- expand.grid(strsplit(gwas_infct[, 5], split = ","))
-gwas_infct_candidate_uniq <- unname(unique(as.matrix(unlist(gwas_infct_candidate)))[, 1])
-gwas_infct_candidate_huri <- gwas_infct_candidate_uniq[gwas_infct_candidate_uniq %in% V(huri_g)$name]
-######
-# GO enrichment analysis
-cluster_list <- lapply(gwas_hit_1st, function(x) {
-    df <- as_data_frame(x)
-    unique(c(df$from, df$to))
-})
-names(cluster_list) <- sort(gwas$All.LD[gwas$All.LD %in% V(huri_g)$name])
+# keep one ortholog at a time
+all_candidate <- expand.grid(strsplit(gwas_ortholog$candidate, split = ","))
 
-bp_cust <- upload_GMT_file(gmtfile = "~/workplace/database/hsapien_HuRI_GOBP_EXP.gmt")
-mf_cust <- upload_GMT_file(gmtfile = "~/workplace/database/hsapien_HuRI_GOMF_EXP.gmt")
-cc_cust <- upload_GMT_file(gmtfile = "~/workplace/database/hsapien_HuRI_GOCC_EXP.gmt")
-## individual subnetwork
-gobp_check <- lapply(cluster_list, function(x) {go2check(x, bp_cust)})
-gomf_check <- lapply(cluster_list, function(x) {go2check(x, mf_cust)})
-gocc_check <- lapply(cluster_list, function(x) {go2check(x, cc_cust)})
-
-bp <- do.call(rbind.data.frame, gobp_check[c(1:12, 14:16)])
-mf <- do.call(rbind.data.frame, gomf_check[c(2, 4:12, 14, 16)])
-cc <- do.call(rbind.data.frame, gocc_check[c(3, 5:7, 9, 12, 14)])
-write.csv(rbind(bp, mf, cc)[, c(1:13, 15:18)], file = "/tmp/GWAS_GO_individual.csv")
-## whole GWAS hits set
-whole_gwas_bp <- go2check(unique(as.character(unlist(lapply(gwas_hit_1st, as_data_frame)))), bp_cust)
-whole_gwas_mf <- go2check(unique(as.character(unlist(lapply(gwas_hit_1st, as_data_frame)))), mf_cust)
-whole_gwas_cc <- go2check(unique(as.character(unlist(lapply(gwas_hit_1st, as_data_frame)))), cc_cust)
-write.csv(rbind(whole_gwas_mf)[, c(1:13, 15:18)], file = "~/Documents/INET-work/virus_network/statistic_results/GWAS/whole_GWAS_GO_v2.csv")
 ######
 # rewiring analysis of HuRI, to see if the HuSCI viral target is significant.
 # load gwas loci info, with 3 phenotype (critical illness, hospitalization and infection)
-gwas_all <- unique(gwas$All.LD)
-gwas_all <- gwas_all[gwas_all != ""]
-
-######
 # subnetwork of GWAS hit from HuRI
 ## inherite from above code
 gwas_all_list_df <- lapply(gwas_hit_1st, as_data_frame)
@@ -114,6 +78,20 @@ gwas_all_df <- do.call(rbind, gwas_all_list_df)
 gwas_all_g_merge <- graph_from_data_frame(gwas_all_df, directed = FALSE)
 ## to have interaction between 1st interactors
 gwas_all_final <- simplify(induced_subgraph(huri_g, names(V(gwas_all_g_merge))), remove.loops = FALSE) # V:118, E:370
+#######
+# keep one ortholog at a time
+ortholog_count <- list()
+for (i in 1:dim(all_candidate)[1]) {
+    ortholog_gra <- combineNetwork(huri_g, c(as.matrix(all_candidate[i, ])))
+    V(ortholog_gra)$color <- ifelse(V(ortholog_gra)$name %in% husci_sym, "red", "grey")
+    name <- paste0(c(as.matrix(all_candidate[i, ])), collapse = "-")
+    ortholog_count[[name]] <- data.frame(Vcount = vcount(ortholog_gra), Ecount = ecount(ortholog_gra))
+    pdf(paste0("/tmp/", name, ".pdf"))
+    plot(ortholog_gra, vertex.size = 4, vertex.label.cex = .5, vertex.label.dist = 1, vertex.label.color = "black")
+    title(name, cex.main = .5)
+    dev.off()
+}
+
 # subgraph for critical illness, hospitalization and infection
 gwas_ctcl_graph <- make_ego_graph(huri_g, nodes = gwas_ctcl_candidate_huri, mode = "all")
 gwas_hosp_graph <- make_ego_graph(huri_g, nodes = gwas_hosp_candidate_huri, mode = "all")
@@ -156,22 +134,6 @@ random_gwas_infct_final[is.na(random_gwas_infct_final)] <- 0
 source("gwas_phenotype_plot.r")
 
 ######
-# 3 phenotypes GO enrichment analysis
-ctcl_gobp <- go2check(unique(as.character(unlist(lapply(gwas_ctcl_graph, as_data_frame)))), bp_cust)
-ctcl_gomf <- go2check(unique(as.character(unlist(lapply(gwas_ctcl_graph, as_data_frame)))), mf_cust)
-ctcl_gocc <- go2check(unique(as.character(unlist(lapply(gwas_ctcl_graph, as_data_frame)))), cc_cust)
-write.csv(rbind(ctcl_gomf)[, c(1:13, 15:18)], file = "/tmp/ctcl_GWAS_go.csv")
-
-hosp_gobp <- go2check(unique(as.character(unlist(lapply(gwas_hosp_graph, as_data_frame)))), bp_cust)
-hosp_gomf <- go2check(unique(as.character(unlist(lapply(gwas_hosp_graph, as_data_frame)))), mf_cust)
-hosp_gocc <- go2check(unique(as.character(unlist(lapply(gwas_hosp_graph, as_data_frame)))), cc_cust)
-write.csv(rbind(hosp_gomf)[, c(1:13, 15:18)], file = "/tmp/hosp_GWAS_go.csv")
-
-infct_gobp <- go2check(unique(as.character(unlist(lapply(gwas_infct_graph, as_data_frame)))), bp_cust)
-infct_gomf <- go2check(unique(as.character(unlist(lapply(gwas_infct_graph, as_data_frame)))), mf_cust)
-infct_gocc <- go2check(unique(as.character(unlist(lapply(gwas_infct_graph, as_data_frame)))), cc_cust)
-write.csv(rbind(infct_gomf, infct_gocc)[, c(1:13, 15:18)], file = "/tmp/infct_GWAS_go.csv")
-######
 # community detected based on 3 different phenotype
 ctcl_network <- combineNetwork(huri_g, gwas_ctcl_candidate_huri)
 ctcl_strong <- components(ctcl_network, mode = "strong")
@@ -196,3 +158,45 @@ plot(gwas_all_final, vertex.size = 3, vertex.label = NA)
 plot(ctcl_network, vertex.size = 3, vertex.label = NA)
 plot(hosp_network, vertex.size = 3, vertex.label = NA)
 plot(infct_network, vertex.size = 3, vertex.label = NA)
+
+######
+# GO enrichment analysis
+cluster_list <- lapply(gwas_hit_1st, function(x) {
+    df <- as_data_frame(x)
+    unique(c(df$from, df$to))
+})
+names(cluster_list) <- sort(gwas$All.LD[gwas$All.LD %in% V(huri_g)$name])
+
+bp_cust <- upload_GMT_file(gmtfile = "~/workplace/database/hsapien_HuRI_GOBP_EXP.gmt")
+mf_cust <- upload_GMT_file(gmtfile = "~/workplace/database/hsapien_HuRI_GOMF_EXP.gmt")
+cc_cust <- upload_GMT_file(gmtfile = "~/workplace/database/hsapien_HuRI_GOCC_EXP.gmt")
+## individual subnetwork
+gobp_check <- lapply(cluster_list, function(x) {go2check(x, bp_cust)})
+gomf_check <- lapply(cluster_list, function(x) {go2check(x, mf_cust)})
+gocc_check <- lapply(cluster_list, function(x) {go2check(x, cc_cust)})
+
+bp <- do.call(rbind.data.frame, gobp_check[c(1:12, 14:16)])
+mf <- do.call(rbind.data.frame, gomf_check[c(2, 4:12, 14, 16)])
+cc <- do.call(rbind.data.frame, gocc_check[c(3, 5:7, 9, 12, 14)])
+write.csv(rbind(bp, mf, cc)[, c(1:13, 15:18)], file = "/tmp/GWAS_GO_individual.csv")
+## whole GWAS hits set
+whole_gwas_bp <- go2check(unique(as.character(unlist(lapply(gwas_hit_1st, as_data_frame)))), bp_cust)
+whole_gwas_mf <- go2check(unique(as.character(unlist(lapply(gwas_hit_1st, as_data_frame)))), mf_cust)
+whole_gwas_cc <- go2check(unique(as.character(unlist(lapply(gwas_hit_1st, as_data_frame)))), cc_cust)
+write.csv(rbind(whole_gwas_mf)[, c(1:13, 15:18)], file = "~/Documents/INET-work/virus_network/statistic_results/GWAS/whole_GWAS_GO_v2.csv")
+######
+# 3 phenotypes GO enrichment analysis
+ctcl_gobp <- go2check(unique(as.character(unlist(lapply(gwas_ctcl_graph, as_data_frame)))), bp_cust)
+ctcl_gomf <- go2check(unique(as.character(unlist(lapply(gwas_ctcl_graph, as_data_frame)))), mf_cust)
+ctcl_gocc <- go2check(unique(as.character(unlist(lapply(gwas_ctcl_graph, as_data_frame)))), cc_cust)
+write.csv(rbind(ctcl_gomf)[, c(1:13, 15:18)], file = "/tmp/ctcl_GWAS_go.csv")
+
+hosp_gobp <- go2check(unique(as.character(unlist(lapply(gwas_hosp_graph, as_data_frame)))), bp_cust)
+hosp_gomf <- go2check(unique(as.character(unlist(lapply(gwas_hosp_graph, as_data_frame)))), mf_cust)
+hosp_gocc <- go2check(unique(as.character(unlist(lapply(gwas_hosp_graph, as_data_frame)))), cc_cust)
+write.csv(rbind(hosp_gomf)[, c(1:13, 15:18)], file = "/tmp/hosp_GWAS_go.csv")
+
+infct_gobp <- go2check(unique(as.character(unlist(lapply(gwas_infct_graph, as_data_frame)))), bp_cust)
+infct_gomf <- go2check(unique(as.character(unlist(lapply(gwas_infct_graph, as_data_frame)))), mf_cust)
+infct_gocc <- go2check(unique(as.character(unlist(lapply(gwas_infct_graph, as_data_frame)))), cc_cust)
+write.csv(rbind(infct_gomf, infct_gocc)[, c(1:13, 15:18)], file = "/tmp/infct_GWAS_go.csv")
